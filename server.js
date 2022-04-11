1e7c8efcff044417bdb2009da90f3b6a2c7047ed
Amir Akhavan Kharazian <AmirKharazian@gmail.com>
2022-04-12 01:13:02 +0430

    add array buf

--- server.js
+++ server.js
@@ -11,11 +11,33 @@ console.log('We are listening on port ' + port)
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.get('/js/kvin.js',function(req,res){
+    res.sendFile(path.join(__dirname + '/node_modules/kvin/kvin.js')); 
+});
 app.get('*', function (req, res) {
 res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.post('/num', function (req, res) {
-    console.log(req.body)
-    return res.end('done')       
+    if (req.headers['content-type'] == 'application/octet-stream') {
+        let msg=[];
+        req.on('data',(chunk)=>{
+            console.log('chunk', chunk);
+            if(chunk){
+                msg.push(chunk);
+            }
+        })
+        req.on('end',()=>{
+            console.log('msg', msg);
+            let buf = Buffer.concat(msg);
+            console.log([].slice.call(buf));
+        })
+        
+    }
+    else {
+        console.log("llll", req.body)
+    }
+    return res.end('done')      
+    
+     
 })
